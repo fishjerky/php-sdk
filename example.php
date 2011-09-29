30bcf2506cd3dd655faa56bfee9d811dc9d59d96
kevyu <kevyu@wayi.com.tw>
2011-09-29 11:55:56 +0800

    remove init.php

--- example.php
+++ example.php
@@ -4,8 +4,9 @@ require 'src/fun.php';
 
 //2.基本設定
 $config = array(
-	'appId'  => '407',                                 //your app id
-	'secret' => '34f7b4bbd93d6de19d0080a2f477bf87',    //you app secret
+	'appId'  => '407',                              //your app id
+	'secret' => '34f7b4bbd93d6de19d0080a2f477bf87'  //you app secret
+	//'redirect_uri' => 'YOUR_URL' 			//implement if your are using for website, and make sure its value is equal the value while setting app
 );
 
 //3.實體化
@@ -20,7 +21,7 @@ if(!$session)
 echo "<h4>取得好友</h4>";
 try {
 	//5.調用api(取得好友)
-	$friends = $fun->Api('/v1/me/friends','GET',array("start"=>0,"count"=>10));
+	$friends = $fun->Api('/v1/me/friends/app/','GET',array("start"=>0,"count"=>10));
 	print_r($friends);
 } catch (ApiException $e) {
 	echo "錯誤代碼：".$e->getCode() . "<br/>";
